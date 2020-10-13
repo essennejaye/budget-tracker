31a01d346bff57da874866c8b27b22a2f2fbfce3
essennejaye <sjeffers02@gmail.com>
2020-10-12 21:11:07 -0700

    added indexedDB

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Budget Tracker Starter Code
+ # Budget Tracker 
+ 
+ ## Description
+ 
+* Giving users a fast and easy way to track their money is important, but allowing them to access that information at any time is even more important. 
+* Having offline functionality is paramount to the success of an application that handles users’ financial information.
+* This project will update an existing budget tracker application to allow for offline functionality. The user will be able to add expenses and deposits to their budget with or without a connection. 
+* If the user enters transactions offline, the total should be updated when they're brought back online. 
+* Once these changes have been made the application will be deployed to Heroku.
+  
+  
---
+++ public/js/idb.js
@@ -0,0 +1,69 @@
+// create variable to hold db connection
+let db;
+// establish connection to IndexedDB
+const request = indexedDB.open('budget_tracker', 1);
+
+// event listener if db version is non-existant or changes
+request.onupgradeneeded = function (event) {
+  const db = event.target.result;
+  // create store to hold data, autoincrement primary key
+  db.createObjectStore('new_budget', { autoIncrement: true });
+};
+
+// successful connection
+request.onsuccess = function (event) {
+  db = event.target.result;
+
+  // check if app is online, upload data if it is
+  if (navigator.onLine) {
+    uploadBudget();
+  }
+};
+
+request.onError = function (event) {
+  console.log(event.target.errorCode);
+};
+
+// function to save data if no internet connection
+function saveRecord(record) {
+  // creates a new record and places it in object store
+  const transaction = db.transaction(['new_budget'], 'readwrite');
+  const budgetObjectStore = transaction.objectStore('new_budget');
+  budgetObjectStore.add(record);
+};
+
+// upload to database when app back online
+function uploadBudget() {
+  const transaction = db.transaction(['new_budget'], 'readwrite');
+  const budgetObjectStore = transaction.objectStore('new_budget');
+  const getAll = budgetObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch('/api/transaction', {
+        method: 'POST',
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: 'application/json, text/plain, */*',
+          'Content-Type': 'application/json'
+        }
+      })
+        .then(response => response.json())
+        .then(serverResponse => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(['new_budget'], 'readwrite');
+          const budgetObjectStore = transaction.objectStore('new_budget');
+          budgetObjectStore.clear();
+
+          alert('All saved budget items have been submitted!');
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  };
+};
+
+window.addEventListener('online', uploadBudget);
